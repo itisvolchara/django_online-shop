2cc2f655e2a8e7e45c0699d6bc97eb1c2ab3ddc7
Vladislav <vladik.kolesnikov.2012@gmail.com>
2024-04-18 16:39:59 +0300

    Separated celery settings from settings.py

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_NAME', 'django_online_shop'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'Minegres123'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': '5432'
     }
@@ -160,29 +160,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BEAT_SCHEDULE = {
-    'send_newsletters': {
-        'task': 'main.tasks.send_newsletters',
-        'schedule': timedelta(minutes=1),
-    },
-}
-
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
-CELERY_BACKEND = 'redis://localhost:6379/1'
-# CELERY_ACCEPT_CONTENT = ['application/json']
-# CELERY_RESULT_SERIALIZER = 'json'
-# CELERY_TASK_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kolkata'
 
 
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-#add your host of the email here in this case its Gmail so we are going to use Gmail host
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-#add the port number of the email server
-EMAIL_PORT = 587
-#add your gamil here
-EMAIL_HOST_USER = 'volcharaodnonogy@gmail.com'
-#add your password here
-EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL='Celery <naincygupta100@gmail.com>'
+
+
+
+
